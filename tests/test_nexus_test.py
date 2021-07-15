7d5251a7d5daef1251d660659f37e57071588853
taras <4751652+tezer@users.noreply.github.com>
2021-07-16 01:01:21 +0300

    simple test

--- tests/test_nexus_test.py
+++ tests/test_nexus_test.py
@@ -1,5 +1,11 @@
 from nexus_test import __version__
+from nexus_test.image_data_processor import spellcheck
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_spellcheck():
+    result = spellcheck("NOT MORE THAN TWO")
+    assert result == "NOT MORE THAN TWO"
